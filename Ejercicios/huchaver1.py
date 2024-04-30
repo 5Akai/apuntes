419ad45b34f0f76cda25e1bcac53692a201a9836
Robee <robehimi2000@gmail.com>
2024-04-30 09:43:48 +0200

    hucha1

---
+++ Ejercicios/huchaver1.py
@@ -0,0 +1,8 @@
+#Preguntamos por la accion: INSERTAR DINERO, ROMPER HUCHA
+#Al romper la hucha, mostramos el importe total
+#Si insertamos: insertar 50, 100, 200, 500 o 1000
+#despues de insertar mostrareá el importe totala y el número de monedas de cada tipo
+
+hucha = {"50":0,"100":0,"200":0,"500":0,"1000":0}
+
+accion = input(f"")
