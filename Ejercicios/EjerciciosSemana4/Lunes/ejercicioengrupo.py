c7a5ef486a15b389d0880349d24d27366ba3ef9e
Roberto Hidalgo <robehimi2000@gmail.com>
2024-04-30 13:28:31 +0200

    Organización III, corrección ejercicio operación

---
+++ Ejercicios/EjerciciosSemana4/Lunes/ejercicioengrupo.py
@@ -0,0 +1,59 @@
+# Encuentas versión 1
+# Preguntar Edad (validar)
+# Preguntar puntuación de 0 a 10 (validar)
+# Preguntar si es el último encuestado
+# Con el último encuestado mostra el número de encuentas y la puntuación media
+
+esUltimo = False
+totalEnc = 0 ## Al final lo voy a usar para hacer los càlculos, ya que la primera encuesta al estar vacía da un valor nulo con el que no se puede 
+
+puntTotal = 0
+listaPunt = []
+respuestaultimo = ""
+
+
+while esUltimo == False:
+     
+    edad = input("Cual es tu edad?: ")
+    
+    while not edad.isdigit():
+        edad = input("Por favor, introduce una edad válida: ")
+        
+        while edad.isdigit():
+            break
+    
+    
+
+    while int(edad) >= 101 or int(edad) <= -1:
+        str(edad) == str(input("Por favor, introduce una edad válida: "))
+  
+            
+    puntuacion = str(input("Cual es tu puntuación?: "))
+   
+    while not puntuacion.isdigit():
+        
+        puntuacion = input("Por favor, introduce una puntuación válida: ")
+        while puntuacion.isdigit():
+              while int(puntuacion) > 10 or int(puntuacion) < 0:
+                puntuacion = str(input("Por favor, introduce una puntuanción válida: "))
+              break
+    
+    
+        
+        
+    
+
+    respuestaultimo = input("Eres el último(S/N): ")
+    puntTotal += int(puntuacion)
+    totalEnc += 1
+    
+
+
+    if respuestaultimo.capitalize() == "S":
+        esUltimo = True
+        print(f"Número de encuestas: {totalEnc}")
+        print(f"La puntuación media es: {puntTotal/totalEnc}\n")
+        
+
+
+
