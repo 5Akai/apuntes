4cfd5fd00835a7b770af2d6450696f42c5b1b320
Roberto Hidalgo <robehimi2000@gmail.com>
2024-05-07 10:38:59 +0200

    yieeeld

--- Ejercicios/EjerciciosSemana5/Martes/Utilizando_Yield.py
+++ Ejercicios/EjerciciosSemana5/Martes/Utilizando_Yield.py
@@ -1,2 +1,39 @@
-##Utilizando Yield##
+#####################################################################
+
+# Utilizando yield                                                  #
+
+#####################################################################
+
+
 numeros = [8, 14, 65, 7, 14, 99, 745, 1, -35, 1408]
+
+
+print(f"Datos: {numeros}")
+
+
+def func1(lista):
+
+    for i in lista:
+
+        return i * 5
+
+    
+
+def func2(lista):
+
+    resultado = []
+
+
+    for i in lista:
+
+        resultado.append(i * 5)
+
+
+    return resultado   
+
+
+print(f"Func 1: {func1(numeros)}")
+
+print(f"Func 2: {func2(numeros)}")
+
+print(f"Lambda: {list(map(lambda x: x * 5, numeros))}")
