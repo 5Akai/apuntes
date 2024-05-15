b77136499f2b50bdd722d06a58db2a2322dab981
Roberto Hidalgo <robehimi2000@gmail.com>
2024-05-15 10:37:00 +0200

    BICIMAD Corregido

--- Ejercicios/EjerciciosSemana6/EjercicioBiciMad.py
+++ Ejercicios/EjerciciosSemana6/EjercicioBiciMad.py
@@ -8,9 +8,12 @@ def InfoBic(item):
     data["ocupadas"] = item["free_bases"]
     data["libres"] = item["dock_bikes"]
     data["calle"] = item["address"]
-    data["total"] = item["free_bases"] + item["dock_bikes"]
+    
 
-    data["mensaje"] = f"Estación: {data['estación']}\n{data['ocupadas']} bicis ocupadas, {data['libres']} estaciones libres, total: {data['total']}"   
+
+    data["mensaje"] = f"Estación: {data['estación']}\n{data['ocupadas']} bicis ocupadas, {data['libres']} Bicis libres\n\n"   
+
+
 
     return(data)
 
@@ -26,56 +29,63 @@ urls = {
 token = ""
 #estación = input("Número de la estación: ")
 
-try:
-    endpoint = urls["base"] + urls["login"]
+##try:
+endpoint = urls["base"] + urls["login"]
 
 
-    headers = {
-        "X-ClientId": "25d3d248-fc0c-479d-8276-78ac52c647f2",
-        "passKey":"141FE2B578702B63F6EE4E03049F95AB594A28BA9B67A7CAFF0D08BDB8B045463A14B6EADF5885D589B00DA11919CB9D12FFC012A317404D1EF97656E67A86B0"
-    }
+headers = {
+    "X-ClientId": "25d3d248-fc0c-479d-8276-78ac52c647f2",
+    "passKey":"141FE2B578702B63F6EE4E03049F95AB594A28BA9B67A7CAFF0D08BDB8B045463A14B6EADF5885D589B00DA11919CB9D12FFC012A317404D1EF97656E67A86B0"
+}
 
-    response = requests.get(endpoint, headers=headers)
-    if(response.status_code == 200):
-        token = response.json()["data"][0]["accessToken"]
-        print(token)
-    else:
-        print(f"Error: ({response.status_code}): {response.reason}")
-        quit()
-
-
-    endpoint2 = urls["base"] + urls["free_Bikes"]
-    headers2 = {"accessToken": token}
-
-    data2 = {
-
-        "cultureInfo": "ES",
-
-        "Text_StopRequired_YN": "Y",
-
-        "Text_EstimationsRequired_YN": "Y",
-
-        "Text_IncidencesRequired_YN": "N",
-
-        "DateTime_Referenced_Incidencies_YYYYMMDD": "20240514"
-
-    }
+response = requests.get(endpoint, headers=headers)
+if(response.status_code == 200):
+    token = response.json()["data"][0]["accessToken"]
+    print(token)
+else:
+    print(f"Error: ({response.status_code}): {response.reason}")
+    quit()
 
 
-    response2 = requests.get(endpoint2, headers=headers2, json=data2)
+endpoint2 = urls["base"] + urls["free_Bikes"]
+headers2 = {"accessToken": token}
 
-    if (response2.status_code == 200):
+data2 = {
 
-        for item in response2.json()["data"]:
-            print(InfoBic(item)["mensaje"])
+    "cultureInfo": "ES",
 
-    else:
-        print(f"Error: ({response2.status_code}): {response2.reason}")
+    "Text_StopRequired_YN": "Y",
+
+    "Text_EstimationsRequired_YN": "Y",
+
+    "Text_IncidencesRequired_YN": "N",
+
+    "DateTime_Referenced_Incidencies_YYYYMMDD": "20240514"
+
+}
+
+
+response2 = requests.get(endpoint2, headers=headers2, json=data2)
+
+if (response2.status_code == 200):
+
+    total = 0
+
+    for item in response2.json()["data"]:
+
+        temp = InfoBic(item)
+        print(temp["mensaje"])
+
+
+        total = total + int(temp["libres"])
+    print(f"Total de bicis en madrid: {total}")
+else:
+    print(f"Error: ({response2.status_code}): {response2.reason}")
 
 
 
-except Exception as e:
-    print(f"Error: {e}")
+##except Exception as e:
+#    print(f"Error: {e}")
 
-input("presione cualquier tecla para salir: ")
-os.system('clear')
+#input("presione cualquier tecla para salir: ")
+#os.system('clear')
--- Microservicios/EMT-Madrid.py
+++ Microservicios/EMT-Madrid.py
@@ -35,56 +35,56 @@ parada = input("Número de la parada: ")
 
 
 #Obtener token de acceso al API
-try:
-    endpoint = urls["base"] + urls["login"]
+##try:
+endpoint = urls["base"] + urls["login"]
 
 
-    headers = {
-        "X-ClientId": "25d3d248-fc0c-479d-8276-78ac52c647f2",
-        "passKey":"141FE2B578702B63F6EE4E03049F95AB594A28BA9B67A7CAFF0D08BDB8B045463A14B6EADF5885D589B00DA11919CB9D12FFC012A317404D1EF97656E67A86B0"
-    }
+headers = {
+    "X-ClientId": "25d3d248-fc0c-479d-8276-78ac52c647f2",
+    "passKey":"141FE2B578702B63F6EE4E03049F95AB594A28BA9B67A7CAFF0D08BDB8B045463A14B6EADF5885D589B00DA11919CB9D12FFC012A317404D1EF97656E67A86B0"
+}
 
-    response = requests.get(endpoint, headers=headers)
-    if(response.status_code == 200):
-        token = response.json()["data"][0]["accessToken"]
-        print(token)
-    else:
-        print(f"Error: ({response.status_code}): {response.reason}")
-        quit()
+response = requests.get(endpoint, headers=headers)
+if(response.status_code == 200):
+    token = response.json()["data"][0]["accessToken"]
+    print(token)
+else:
+    print(f"Error: ({response.status_code}): {response.reason}")
+    quit()
 
-    endpoint2 = urls["base"] + urls["timeArrivalBus"].replace("<stopId>", parada)
-    headers2 = {"accessToken": token}
-    data2 = {
+endpoint2 = urls["base"] + urls["timeArrivalBus"].replace("<stopId>", parada)
+headers2 = {"accessToken": token}
+data2 = {
 
-        "cultureInfo": "ES",
+    "cultureInfo": "ES",
 
-        "Text_StopRequired_YN": "Y",
+    "Text_StopRequired_YN": "Y",
 
-        "Text_EstimationsRequired_YN": "Y",
+    "Text_EstimationsRequired_YN": "Y",
 
-        "Text_IncidencesRequired_YN": "N",
+    "Text_IncidencesRequired_YN": "N",
 
-        "DateTime_Referenced_Incidencies_YYYYMMDD": "20240514"
+    "DateTime_Referenced_Incidencies_YYYYMMDD": "20240514"
 
-    }
+}
 
-    response2 = requests.post(endpoint2, headers=headers2, json=data2)
-    #OTRA OPCIÓN#response2 = requests.post(endpoint2, headers=headers2, data=json.dumps(data2))
+response2 = requests.post(endpoint2, headers=headers2, json=data2)
+#OTRA OPCIÓN#response2 = requests.post(endpoint2, headers=headers2, data=json.dumps(data2))
 
-    if (response2.status_code == 200):
+if (response2.status_code == 200):
 
-        for item in response2.json()["data"][0]["Arrive"]:
-            print(InfoBus(item)["mensaje"])
+    for item in response2.json()["data"][0]["Arrive"]:
+        print(InfoBus(item)["mensaje"])
 
-    else:
-        print(f"Error: ({response2.status_code}): {response2.reason}")
-
-
-
+else:
+    print(f"Error: ({response2.status_code}): {response2.reason}")
 
 
 
-except Exception as e:
+
+
+
+##except Exception as e:
     print(f"Error: {e}")
 
 input("presione cualquier tecla para salir: ")
