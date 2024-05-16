1c11d3d41f6aafb243584adaca2409941f0788c7
Robee <robehimi2000@gmail.com>
2024-05-16 10:38:09 +0200

    si

--- Microservicios/05.06.EMT-web.py
+++ Microservicios/05.06.EMT-web.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, render_template_string
-
+import EMTModulo
 #########################################################################
 # Creamos una instancia de Flask
 #########################################################################
