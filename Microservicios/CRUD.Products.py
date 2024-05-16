f080bd9a6e28b43641f06833bc09a3eb2844f13c
Robee <robehimi2000@gmail.com>
2024-05-16 12:17:06 +0200

    Crudborja

--- Microservicios/CRUD.Products.py
+++ Microservicios/CRUD.Products.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, render_template_string, request, jsonify
+from flask import Flask, render_template, request, jsonify
 import pymssql
 
 #########################################################################
@@ -15,20 +15,13 @@ app = Flask(__name__, template_folder="templates")
 # Ruta: http://dominio.com/api/products
 @app.route("/api/products", methods=["GET"])
 def products_get():
-
-    try: 
+    try:
         connection = pymssql.connect(
-
-        server="hostdb2-eoi.database.windows.net",
-
-        port="1433",
-
-        user="Administrador",
-
-        password="azurePa$$w0rd",
-
-        database="Northwind")
-
+            server="hostdb2-eoi.database.windows.net",
+            port="1433",
+            user="Administrador",
+            password="azurePa$$w0rd",
+            database="Northwind")
 
         cursor = connection.cursor(as_dict=True)
         cursor.execute("SELECT * FROM dbo.Products")
@@ -37,104 +30,70 @@ def products_get():
     except Exception as err:
         return jsonify(err), 500
 
-
-
 # Retorna los datos del producto 34
-# Ruta http://dominio.com/api/products/34
-@app.route("/api/products/<int:id>", methods=["GET"])
-def product34_get(id):
+# Ruta: http://dominio.com/api/products/34
+# @app.route("/api/products/<int:id>", methods=["GET"])
+@app.route("/api/products/<id>", methods=["GET"])
+def product_get(id):
     try:
-        if (id.isdigit()): 
+        if (id.isdigit()):
             connection = pymssql.connect(
-
-            server="hostdb2-eoi.database.windows.net",
-
-            port="1433",
-
-            user="Administrador",
-
-            password="azurePa$$w0rd",
-
-            database="Northwind")
-
+                server="hostdb2-eoi.database.windows.net",
+                port="1433",
+                user="Administrador",
+                password="azurePa$$w0rd",
+                database="Northwind")
 
             cursor = connection.cursor(as_dict=True)
-            cursor.execute("SELECT * FROM dbo.Products")
+            cursor.execute(f"SELECT * FROM dbo.Products WHERE ProductID = {id}")
 
-            connection.close()
-
-            return jsonify(cursor.fetchall()), 200
+            return jsonify(cursor.fetchone()), 200
         else:
-            return jsonify({"Message": "La referencia del producto no es válida"} ), 400
-    
+            return jsonify({"Message": "La referencia del producto no es valida."}), 400
     except Exception as err:
         return jsonify(err), 500
 
-
-## Crear nuevo producto
-
+# Crear un nuevo producto
+# Ruta: http://dominio.com/api/products
 @app.route("/api/products", methods=["POST"])
 def products_post():
-
-    try: 
-
+    try:
+        new_product = request.json
+        
         connection = pymssql.connect(
-
             server="hostdb2-eoi.database.windows.net",
-
             port="1433",
-
             user="Administrador",
-
             password="azurePa$$w0rd",
-
             database="Northwind")
-
 
         cursor = connection.cursor(as_dict=True)
 
-        new_product = request.json
         command = f"""
-
-            INSERT INTO dbo.Products(ProductID, ProductName, CategoryID, Discontinued, SupplierID
-
-                ReorderLevel, QuantityPerUnit, UnitsInStock, UnitsOnOrder, UnitPrice) VALUES(
-
-                {new_product["ProductID"]},
-
+            INSERT INTO dbo.Products(ProductName, CategoryID, Discontinued, SupplierID,
+                ReorderLevel, QuantityPerUnit, UnitsInStock, UnitsOnOrder, UnitPrice) VALUES (
                 '{new_product["ProductName"]}',
-
                 {new_product["CategoryID"]},
-
-                {new_product["Discontinued"]},
-
+                '{new_product["Discontinued"]}',
                 {new_product["SupplierID"]},
-
                 {new_product["ReorderLevel"]},
-
                 '{new_product["QuantityPerUnit"]}',
-
                 {new_product["UnitsInStock"]},
-
                 {new_product["UnitsOnOrder"]},
-
                 {new_product["UnitPrice"]})
-
         """
+
         cursor.execute(command)
         connection.commit()
 
-        if (cursor.rowcount == 1):
+        if(cursor.rowcount == 1):
             return jsonify(new_product), 201
         else:
-            return jsonify({"Message": "Producto no insertado"} ), 400
-   
+            return jsonify({"Message": "Producto no insertado."}), 400
     except Exception as err:
         return jsonify(err), 500
 
-
-
 #########################################################################
-# Ejecuar la aplicación de Flask en el servidor web integrado
+# Ejecutar la aplicación de Flask en el servidor web integrado
 #########################################################################
 app.run()
