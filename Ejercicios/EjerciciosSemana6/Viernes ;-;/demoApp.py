4ab63d667a54f9b637f42d90858ae98d16179f08
Robee <robehimi2000@gmail.com>
2024-05-17 13:23:52 +0200

    error

--- Ejercicios/EjerciciosSemana6/Viernes ;-;/demoApp.py
+++ Ejercicios/EjerciciosSemana6/Viernes ;-;/demoApp.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response, jsonify, session, render_template
+from flask import Flask, request, Response, jsonify, session
 from demoModulo import *
 
 #########################################################################
@@ -11,22 +11,46 @@ app = Flask(__name__, template_folder="templates")
 # Rutas de la aplicación Flask
 #########################################################################
 
-# Ruta: http://dominio.com/
-@app.route("/", methods=["GET"])
-def index():
-    return f'<h1 style="color: teal;">Hola Mundo !!!</h1>'
-
-
-
 @app.route("/api/customers", methods=["GET"])
-def customers():
+def get_customers():
+    # return Response(Get_Customers_List(), status=200, content_type="application/json")
     return jsonify(Get_Customers_List()), 200
 
-
-
-@app.route("/api/customers/<string:id>", methods=["GET"])
-def clist():
+@app.route("/api/customers/<id>", methods=["GET"])
+def get_customer(id):
+    # return Response(Get_Customer(id), status=200, content_type="application/json")
     return jsonify(Get_Customer(id)), 200
+
+@app.route("/api/orders", methods=["GET"])
+def Get_Orders_List():
+    # return Response(Get_Customers_List(), status=200, content_type="application/json")
+    return jsonify(Get_Orders_List()), 200
+
+@app.route("/api/orders/<id>", methods=["GET"])
+def Get_Orders(id):
+    # return Response(Get_Customer(id), status=200, content_type="application/json")
+    return jsonify(Get_Orders(id)), 200
+
+
+#########################################################################
+# Control de errores aplicación Flask
+#########################################################################
+
+# Código para manejar la solicitud no encontrada
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({f"Error {e.code} ({e.name})": f"{e.description}"}), 404
+
+# Código para manejar el error interno de código
+@app.errorhandler(500)
+def internal_server_error(e):
+    return jsonify({f"Error {e.code} ({e.name})": f"{e.description}"}), 500
+
+# Código para manejar errores genericos
+@app.errorhandler(Exception)
+def handle_error(e):
+    return jsonify({f"Error": f"{e}"}), 500
+
 
 #########################################################################
 # Ejecutar la aplicación de Flask en el servidor web integrado
--- Ejercicios/EjerciciosSemana6/Viernes ;-;/demoModulo.py
+++ Ejercicios/EjerciciosSemana6/Viernes ;-;/demoModulo.py
@@ -1,23 +1,38 @@
 from pymongo import MongoClient, collection
-from flask import request
 from bson.json_util import dumps, loads
 
 server = "localhost"
-
 port = 27017
-
 db = "northwind"
 
-def Get_Customers_List() -> str:
+def Get_Customers_List() -> list:
     try:
-        return list(Get_Customers_List("customers").find{}, {"_id": 0})
+       
+        return list(_Get_Collection("customers").find({}, {"_id": 0}))
     except Exception as e:
-        return{"Error": e}
+        return {"Error": e}
 
-def Get_Customer(id) -> str:
+def Get_Customer(id) -> dict:
     try:
-        return __Get_Collection("customers").find_one{"CustomerID": id}, {"_id": 0}))
-    except Exception as e:
-        return{"Error": e}
     
-def __Get_Collection(collection, server=server, port=port, )
+        return _Get_Collection("customers").find_one({"CustomerID": id}, {"_id": 0})
+    except Exception as e:
+        return {"Error": e}
+    
+def Get_Orders_List() -> list:
+    try:
+       
+        return list(_Get_Collection("orders").find({}, {"_id": 0}))
+    except Exception as e:
+        return {"Error": e}
+
+def Get_Orders(id) -> dict:
+    try:
+       
+        return _Get_Collection("orders").find_one({"OrderID": id}, {"_id": 0})
+    except Exception as e:
+        return {"Error": e}
+
+
+def _Get_Collection(collection, server=server, port=port, db=db):
+    return MongoClient(f"mongodb://{server}:{port}/")[db][collection]
